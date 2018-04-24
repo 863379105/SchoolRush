5b570ac76abd35aef0c607818488c2084dcfcddb
Onnow <863379105@qq.com>
2018-04-24 12:23:43 +0800

    修改问题提交返回页面，添加了分享借口

--- src/views/questions/submitQuestion.vue
+++ src/views/questions/submitQuestion.vue
@@ -2,22 +2,27 @@
     <div id="submit-panel">
         <Row>
             <Col span="24">
-                <a href="###" id="question-titel">我的问题</a>
+                <router-link :to="'question/'+id" 
+                    id="question-title">
+                    {{title}}</router-link>
             </Col>
         </Row>
         <Row>
             <Col span="24">
                 <Icon type="checkmark-round" size = "15" color = "#2d8cf0"></Icon>
-                <a href="123" id="question-msg">发布成功，点击查看问题</a>
+                <router-link :to="'question/'+id" 
+                    id="question-msg">
+                    提交成功，点击查看问题</router-link>
             </Col>
         </Row>
         <Row>
             <Col span="24">
                 <ul id="share-list">
-                    <li><Button type="primary" shape="circle" size="large" ><span class="question-share">复制链接</span></Button></li>
-                    <li><Button type="primary" shape="circle" size="large" ><span class="question-share">复制链接</span></Button></li>
-                    <li><Button type="primary" shape="circle" size="large" ><span class="question-share">复制链接</span></Button></li>
-                    <li><Button type="primary" shape="circle" size="large" ><span class="question-share">复制链接</span></Button></li>
+                    <li><Button type="primary" shape="circle" size="large" @click="shareToWB"><span class="question-share">微博分享</span></Button></li>
+                    <li><Button type="primary" shape="circle" size="large" @click="shareToDB"><span class="question-share">豆瓣分享</span></Button></li>
+                    <li><Button type="primary" shape="circle" size="large" @click="shareToQZoom"><span class="question-share">空间分享</span></Button></li>                    
+                    <li><Button type="primary" shape="circle" size="large" @click="copySelf"><span class="question-share">复制链接</span></Button></li>
+                    <input type="text" id="question-url">
                 </ul>
             </Col>
         </Row>
@@ -32,8 +37,27 @@
 <script>
 export default {
   props:[
-
-  ]
+        "title",
+        "id",
+  ],
+  methods:{  
+      shareToQZoom(){
+          window.open('https://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=www.schoolrush.top/qustion/' + this.id, '_blank')
+      },
+      shareToWB(){
+          window.open('http://service.weibo.com/share/share.php?appkey=1881139527&url=https://www.schoolrush.top/qustion/' + this.id + '&title=' + this.title, '_blank')
+      },
+      shareToDB(){
+          window.open('https://www.douban.com/share/service?name=' + this.title +'&url=www.schoolrush.top/qustion/' + this.id, '_blank')
+      },
+      copySelf(){   
+            var input = document.getElementById("question-url");
+            input.value = 'www.schoolrush.top/question/' + this.id; // 修改文本框的内容
+            input.select(); // 选中文本
+            document.execCommand("copy"); // 执行浏览器复制命令
+            alert("复制成功");
+        }
+    }
 }
 </script>
 <style>
@@ -43,7 +67,7 @@ export default {
         color: #495060;
         
     }
-    #question-titel{
+    #question-title{
         font-weight: bolder;
         color: black;   
         font-size: 1.75em;
@@ -71,5 +95,10 @@ export default {
         height: 8rem;
         border-radius: 50% 50% 0 0;
     }
+    #question-url{
+        position: absolute;
+        top: -9999px;
+        left: -9999px; 
+    } 
 </style>
 
